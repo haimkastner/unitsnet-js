6e5e47849dc422bff0fe7401254ac6daff2ee62d
Haim Kastner <haim.kastner@gmail.com>
2019-12-30 15:07:08 +0200

    Fetch definitions from the unitsnet repo (#10)
    * Remove local units definitions #2
    
    * Fetch units definitions from the original angularsen unitsnet repo
    https://github.com/angularsen/UnitsNet/pull/730 #2

--- generator-scripts/src/index.ts
+++ generator-scripts/src/index.ts
@@ -4,6 +4,7 @@ import { Project } from 'ts-morph';
 import { generateUnitsModuleExport } from './export-units-generator';
 import { generateUnitsFromUnitsDefinitions } from './units-generator';
 import { generateDocumentation } from './documentation-generatoe';
+import { fetchUnitsDefinitions } from './units-definitions-fetcher';
 
 const unitsJs = `
 8 8888      88 b.             8  8 8888 8888888 8888888888 d888888o.             8 8888   d888888o.   
@@ -21,22 +22,11 @@ const unitsJs = `
 
 console.info(unitsJs);
 
-const unitsDefinitionsDirectory = '../UnitDefinitions';
 const unitsDocumentationDefinitionsDirectory = '..';
 const unitsTestinationDirectory = '../src'
 
-// Read the definition JSON files
-const unitsFiles = fse.readdirSync(unitsDefinitionsDirectory);
-
-const unitsDefinitions: UnitTypeDefinition[] = [];
-
-// Load the difinition files
-for (const unitFile of unitsFiles) {
-    console.info(`Reading & Parsing '${unitFile}' File ...`);
-
-    const unitDefinition = fse.readJSONSync(`${unitsDefinitionsDirectory}/${unitFile}`) as unknown as UnitTypeDefinition;
-    unitsDefinitions.push(unitDefinition);
-}
+// Fetch the latest units definitions from the unitsnet repo
+const unitsDefinitions: UnitTypeDefinition[] = fetchUnitsDefinitions('angularsen/UnitsNet');
 
 console.info(`Generating Units ...`);
 
---
+++ generator-scripts/src/units-definitions-fetcher.ts
@@ -0,0 +1,48 @@
+import { UnitTypeDefinition } from "./models/units-definition";
+import request from 'sync-request';
+
+const githubOptions = {
+    headers: {
+        'user-agent': 'PostmanRuntime/7.20.1',
+    },
+};
+
+/**
+ * 
+ * @param repoOwnerAndName The UnitsNEt repo owner and name
+ * @returns The units definition array
+ */
+export function fetchUnitsDefinitions(repoOwnerAndName: string): UnitTypeDefinition[] {
+
+    const directoryUrl = `https://api.github.com/repos/${repoOwnerAndName}/contents/Common/UnitDefinitions`;
+    const filesUrl = `https://raw.githubusercontent.com/${repoOwnerAndName}/master/Common/UnitDefinitions`;
+
+    console.info(`Fetching units files list...`);
+
+    try {
+
+        const response = request('GET', directoryUrl, githubOptions);
+
+        const body = JSON.parse(response.body.toString('utf-8')) as unknown as [];
+
+        const files = body.map((file: any) => file.name);
+
+        const unitsDefinition: UnitTypeDefinition[] = [];
+
+        for (const file of files) {
+            try {
+                console.info(`Fetching ${file} file...`);
+                const response = request('GET', `${filesUrl}/${file}`, githubOptions);
+                const rawBody = response.body.toString('utf-8');
+                unitsDefinition.push(JSON.parse(rawBody) as unknown as UnitTypeDefinition);
+            } catch (error) {
+                console.warn(`Fetching ${file} file failed or file damaged, ${error}`);
+            }
+        }
+        return unitsDefinition;
+
+    } catch (error) {
+        console.warn(`Fetching files failed or data damaged, ${error}`);
+        process.exit(1);
+    }
+}
