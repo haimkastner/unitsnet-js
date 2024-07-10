c780af17c2718f53241b8d1b2f3215617934b313
haimkastner <haimkastner@users.noreply.github.com>
2024-07-10 17:26:31 +0000

    Update gen docs for version 3.0.9 [skip-ci]
    Files changed:
    M	src/gen-units/lineardensity.g.ts

--- src/gen-units/lineardensity.g.ts
+++ src/gen-units/lineardensity.g.ts
@@ -21,6 +21,8 @@ export enum LinearDensityUnits {
     /** */
     PoundsPerFoot = "PoundPerFoot",
     /** */
+    GramsPerFoot = "GramPerFoot",
+    /** */
     MicrogramsPerMillimeter = "MicrogramPerMillimeter",
     /** */
     MilligramsPerMillimeter = "MilligramPerMillimeter",
@@ -37,7 +39,13 @@ export enum LinearDensityUnits {
     /** */
     MilligramsPerMeter = "MilligramPerMeter",
     /** */
-    KilogramsPerMeter = "KilogramPerMeter"
+    KilogramsPerMeter = "KilogramPerMeter",
+    /** */
+    MicrogramsPerFoot = "MicrogramPerFoot",
+    /** */
+    MilligramsPerFoot = "MilligramPerFoot",
+    /** */
+    KilogramsPerFoot = "KilogramPerFoot"
 }
 
 /** The Linear Density, or more precisely, the linear mass density, of a substance is its mass per unit length.  The term linear density is most often used when describing the characteristics of one-dimensional objects, although linear density can also be used to describe the density of a three-dimensional quantity along one particular dimension. */
@@ -48,6 +56,7 @@ export class LinearDensity extends BaseUnit {
     private gramspermeterLazy: number | null = null;
     private poundsperinchLazy: number | null = null;
     private poundsperfootLazy: number | null = null;
+    private gramsperfootLazy: number | null = null;
     private microgramspermillimeterLazy: number | null = null;
     private milligramspermillimeterLazy: number | null = null;
     private kilogramspermillimeterLazy: number | null = null;
@@ -57,6 +66,9 @@ export class LinearDensity extends BaseUnit {
     private microgramspermeterLazy: number | null = null;
     private milligramspermeterLazy: number | null = null;
     private kilogramspermeterLazy: number | null = null;
+    private microgramsperfootLazy: number | null = null;
+    private milligramsperfootLazy: number | null = null;
+    private kilogramsperfootLazy: number | null = null;
 
     /**
      * Create a new LinearDensity.
@@ -124,6 +136,14 @@ export class LinearDensity extends BaseUnit {
             return this.poundsperfootLazy;
         }
         return this.poundsperfootLazy = this.convertFromBase(LinearDensityUnits.PoundsPerFoot);
+    }
+
+    /** */
+    public get GramsPerFoot(): number {
+        if(this.gramsperfootLazy !== null){
+            return this.gramsperfootLazy;
+        }
+        return this.gramsperfootLazy = this.convertFromBase(LinearDensityUnits.GramsPerFoot);
     }
 
     /** */
@@ -198,6 +218,30 @@ export class LinearDensity extends BaseUnit {
         return this.kilogramspermeterLazy = this.convertFromBase(LinearDensityUnits.KilogramsPerMeter);
     }
 
+    /** */
+    public get MicrogramsPerFoot(): number {
+        if(this.microgramsperfootLazy !== null){
+            return this.microgramsperfootLazy;
+        }
+        return this.microgramsperfootLazy = this.convertFromBase(LinearDensityUnits.MicrogramsPerFoot);
+    }
+
+    /** */
+    public get MilligramsPerFoot(): number {
+        if(this.milligramsperfootLazy !== null){
+            return this.milligramsperfootLazy;
+        }
+        return this.milligramsperfootLazy = this.convertFromBase(LinearDensityUnits.MilligramsPerFoot);
+    }
+
+    /** */
+    public get KilogramsPerFoot(): number {
+        if(this.kilogramsperfootLazy !== null){
+            return this.kilogramsperfootLazy;
+        }
+        return this.kilogramsperfootLazy = this.convertFromBase(LinearDensityUnits.KilogramsPerFoot);
+    }
+
     /**
      * Create a new LinearDensity instance from a GramsPerMillimeter
      *
@@ -246,6 +290,16 @@ export class LinearDensity extends BaseUnit {
      */
     public static FromPoundsPerFoot(value: number): LinearDensity {
         return new LinearDensity(value, LinearDensityUnits.PoundsPerFoot);
+    }
+
+    /**
+     * Create a new LinearDensity instance from a GramsPerFoot
+     *
+     * @param value The unit as GramsPerFoot to create a new LinearDensity from.
+     * @returns The new LinearDensity instance.
+     */
+    public static FromGramsPerFoot(value: number): LinearDensity {
+        return new LinearDensity(value, LinearDensityUnits.GramsPerFoot);
     }
 
     /**
@@ -339,6 +393,36 @@ export class LinearDensity extends BaseUnit {
     }
 
     /**
+     * Create a new LinearDensity instance from a MicrogramsPerFoot
+     *
+     * @param value The unit as MicrogramsPerFoot to create a new LinearDensity from.
+     * @returns The new LinearDensity instance.
+     */
+    public static FromMicrogramsPerFoot(value: number): LinearDensity {
+        return new LinearDensity(value, LinearDensityUnits.MicrogramsPerFoot);
+    }
+
+    /**
+     * Create a new LinearDensity instance from a MilligramsPerFoot
+     *
+     * @param value The unit as MilligramsPerFoot to create a new LinearDensity from.
+     * @returns The new LinearDensity instance.
+     */
+    public static FromMilligramsPerFoot(value: number): LinearDensity {
+        return new LinearDensity(value, LinearDensityUnits.MilligramsPerFoot);
+    }
+
+    /**
+     * Create a new LinearDensity instance from a KilogramsPerFoot
+     *
+     * @param value The unit as KilogramsPerFoot to create a new LinearDensity from.
+     * @returns The new LinearDensity instance.
+     */
+    public static FromKilogramsPerFoot(value: number): LinearDensity {
+        return new LinearDensity(value, LinearDensityUnits.KilogramsPerFoot);
+    }
+
+    /**
      * Gets the base unit enumeration associated with LinearDensity
      * @returns The unit enumeration that can be used to interact with this type
      */
@@ -385,6 +469,7 @@ export class LinearDensity extends BaseUnit {
             case LinearDensityUnits.GramsPerMeter: return this.GramsPerMeter;
             case LinearDensityUnits.PoundsPerInch: return this.PoundsPerInch;
             case LinearDensityUnits.PoundsPerFoot: return this.PoundsPerFoot;
+            case LinearDensityUnits.GramsPerFoot: return this.GramsPerFoot;
             case LinearDensityUnits.MicrogramsPerMillimeter: return this.MicrogramsPerMillimeter;
             case LinearDensityUnits.MilligramsPerMillimeter: return this.MilligramsPerMillimeter;
             case LinearDensityUnits.KilogramsPerMillimeter: return this.KilogramsPerMillimeter;
@@ -394,6 +479,9 @@ export class LinearDensity extends BaseUnit {
             case LinearDensityUnits.MicrogramsPerMeter: return this.MicrogramsPerMeter;
             case LinearDensityUnits.MilligramsPerMeter: return this.MilligramsPerMeter;
             case LinearDensityUnits.KilogramsPerMeter: return this.KilogramsPerMeter;
+            case LinearDensityUnits.MicrogramsPerFoot: return this.MicrogramsPerFoot;
+            case LinearDensityUnits.MilligramsPerFoot: return this.MilligramsPerFoot;
+            case LinearDensityUnits.KilogramsPerFoot: return this.KilogramsPerFoot;
 
             default:
                 break;
@@ -409,6 +497,10 @@ export class LinearDensity extends BaseUnit {
                 case LinearDensityUnits.GramsPerMeter: return super.internalDivide(this.value, 1e-3);
                 case LinearDensityUnits.PoundsPerInch: return super.internalMultiply(this.value, 5.5997415e-2);
                 case LinearDensityUnits.PoundsPerFoot: return super.internalDivide(this.value, 1.48816394);
+                case LinearDensityUnits.GramsPerFoot: {
+                    const v4 = super.internalDivide(1e-3, 0.3048);
+                    return super.internalDivide(this.value, v4);
+                }
                 case LinearDensityUnits.MicrogramsPerMillimeter: return super.internalDivide(this.value, 0.000001);
                 case LinearDensityUnits.MilligramsPerMillimeter: return super.internalDivide(this.value, 0.001);
                 case LinearDensityUnits.KilogramsPerMillimeter: return super.internalDivide(this.value, 1000);
@@ -436,6 +528,21 @@ export class LinearDensity extends BaseUnit {
                     const v3 = super.internalDivide(this.value, 1e-3);
                     return super.internalDivide(v3, 1000);
                 }
+                case LinearDensityUnits.MicrogramsPerFoot: {
+                    const v4 = super.internalDivide(1e-3, 0.3048);
+                    const v5 = super.internalDivide(this.value, v4);
+                    return super.internalDivide(v5, 0.000001);
+                }
+                case LinearDensityUnits.MilligramsPerFoot: {
+                    const v4 = super.internalDivide(1e-3, 0.3048);
+                    const v5 = super.internalDivide(this.value, v4);
+                    return super.internalDivide(v5, 0.001);
+                }
+                case LinearDensityUnits.KilogramsPerFoot: {
+                    const v4 = super.internalDivide(1e-3, 0.3048);
+                    const v5 = super.internalDivide(this.value, v4);
+                    return super.internalDivide(v5, 1000);
+                }
                 default: return Number.NaN;
             }
         switch (toUnit) {
@@ -444,6 +551,7 @@ export class LinearDensity extends BaseUnit {
             case LinearDensityUnits.GramsPerMeter: return this.value / 1e-3;
             case LinearDensityUnits.PoundsPerInch: return this.value * 5.5997415e-2;
             case LinearDensityUnits.PoundsPerFoot: return this.value / 1.48816394;
+            case LinearDensityUnits.GramsPerFoot: return this.value / ( 1e-3 / 0.3048 );
             case LinearDensityUnits.MicrogramsPerMillimeter: return (this.value) / 0.000001;
             case LinearDensityUnits.MilligramsPerMillimeter: return (this.value) / 0.001;
             case LinearDensityUnits.KilogramsPerMillimeter: return (this.value) / 1000;
@@ -453,6 +561,9 @@ export class LinearDensity extends BaseUnit {
             case LinearDensityUnits.MicrogramsPerMeter: return (this.value / 1e-3) / 0.000001;
             case LinearDensityUnits.MilligramsPerMeter: return (this.value / 1e-3) / 0.001;
             case LinearDensityUnits.KilogramsPerMeter: return (this.value / 1e-3) / 1000;
+            case LinearDensityUnits.MicrogramsPerFoot: return (this.value / ( 1e-3 / 0.3048 )) / 0.000001;
+            case LinearDensityUnits.MilligramsPerFoot: return (this.value / ( 1e-3 / 0.3048 )) / 0.001;
+            case LinearDensityUnits.KilogramsPerFoot: return (this.value / ( 1e-3 / 0.3048 )) / 1000;
             default: return Number.NaN;
         }
     }
@@ -465,6 +576,10 @@ export class LinearDensity extends BaseUnit {
                 case LinearDensityUnits.GramsPerMeter: return super.internalMultiply(value, 1e-3);
                 case LinearDensityUnits.PoundsPerInch: return super.internalDivide(value, 5.5997415e-2);
                 case LinearDensityUnits.PoundsPerFoot: return super.internalMultiply(value, 1.48816394);
+                case LinearDensityUnits.GramsPerFoot: {
+                    const v4 = super.internalDivide(1e-3, 0.3048);
+                    return super.internalMultiply(value, v4);
+                }
                 case LinearDensityUnits.MicrogramsPerMillimeter: return super.internalMultiply(value, 0.000001);
                 case LinearDensityUnits.MilligramsPerMillimeter: return super.internalMultiply(value, 0.001);
                 case LinearDensityUnits.KilogramsPerMillimeter: return super.internalMultiply(value, 1000);
@@ -492,6 +607,21 @@ export class LinearDensity extends BaseUnit {
                     const v3 = super.internalMultiply(value, 1e-3);
                     return super.internalMultiply(v3, 1000);
                 }
+                case LinearDensityUnits.MicrogramsPerFoot: {
+                    const v4 = super.internalDivide(1e-3, 0.3048);
+                    const v5 = super.internalMultiply(value, v4);
+                    return super.internalMultiply(v5, 0.000001);
+                }
+                case LinearDensityUnits.MilligramsPerFoot: {
+                    const v4 = super.internalDivide(1e-3, 0.3048);
+                    const v5 = super.internalMultiply(value, v4);
+                    return super.internalMultiply(v5, 0.001);
+                }
+                case LinearDensityUnits.KilogramsPerFoot: {
+                    const v4 = super.internalDivide(1e-3, 0.3048);
+                    const v5 = super.internalMultiply(value, v4);
+                    return super.internalMultiply(v5, 1000);
+                }
                 default: return Number.NaN;
             }
         switch (fromUnit) {
@@ -500,6 +630,7 @@ export class LinearDensity extends BaseUnit {
             case LinearDensityUnits.GramsPerMeter: return value * 1e-3;
             case LinearDensityUnits.PoundsPerInch: return value / 5.5997415e-2;
             case LinearDensityUnits.PoundsPerFoot: return value * 1.48816394;
+            case LinearDensityUnits.GramsPerFoot: return value * ( 1e-3 / 0.3048 );
             case LinearDensityUnits.MicrogramsPerMillimeter: return (value) * 0.000001;
             case LinearDensityUnits.MilligramsPerMillimeter: return (value) * 0.001;
             case LinearDensityUnits.KilogramsPerMillimeter: return (value) * 1000;
@@ -509,6 +640,9 @@ export class LinearDensity extends BaseUnit {
             case LinearDensityUnits.MicrogramsPerMeter: return (value * 1e-3) * 0.000001;
             case LinearDensityUnits.MilligramsPerMeter: return (value * 1e-3) * 0.001;
             case LinearDensityUnits.KilogramsPerMeter: return (value * 1e-3) * 1000;
+            case LinearDensityUnits.MicrogramsPerFoot: return (value * ( 1e-3 / 0.3048 )) * 0.000001;
+            case LinearDensityUnits.MilligramsPerFoot: return (value * ( 1e-3 / 0.3048 )) * 0.001;
+            case LinearDensityUnits.KilogramsPerFoot: return (value * ( 1e-3 / 0.3048 )) * 1000;
             default: return Number.NaN;
         }
     }
@@ -535,6 +669,8 @@ export class LinearDensity extends BaseUnit {
                 return super.truncateFractionDigits(this.PoundsPerInch, fractionalDigits) + ` lb/in`;
             case LinearDensityUnits.PoundsPerFoot:
                 return super.truncateFractionDigits(this.PoundsPerFoot, fractionalDigits) + ` lb/ft`;
+            case LinearDensityUnits.GramsPerFoot:
+                return super.truncateFractionDigits(this.GramsPerFoot, fractionalDigits) + ` g/ft`;
             case LinearDensityUnits.MicrogramsPerMillimeter:
                 return super.truncateFractionDigits(this.MicrogramsPerMillimeter, fractionalDigits) + ` μg/mm`;
             case LinearDensityUnits.MilligramsPerMillimeter:
@@ -553,6 +689,12 @@ export class LinearDensity extends BaseUnit {
                 return super.truncateFractionDigits(this.MilligramsPerMeter, fractionalDigits) + ` mg/m`;
             case LinearDensityUnits.KilogramsPerMeter:
                 return super.truncateFractionDigits(this.KilogramsPerMeter, fractionalDigits) + ` kg/m`;
+            case LinearDensityUnits.MicrogramsPerFoot:
+                return super.truncateFractionDigits(this.MicrogramsPerFoot, fractionalDigits) + ` μg/ft`;
+            case LinearDensityUnits.MilligramsPerFoot:
+                return super.truncateFractionDigits(this.MilligramsPerFoot, fractionalDigits) + ` mg/ft`;
+            case LinearDensityUnits.KilogramsPerFoot:
+                return super.truncateFractionDigits(this.KilogramsPerFoot, fractionalDigits) + ` kg/ft`;
         default:
             break;
         }
@@ -580,6 +722,8 @@ export class LinearDensity extends BaseUnit {
                 return `lb/in`;
             case LinearDensityUnits.PoundsPerFoot:
                 return `lb/ft`;
+            case LinearDensityUnits.GramsPerFoot:
+                return `g/ft`;
             case LinearDensityUnits.MicrogramsPerMillimeter:
                 return `μg/mm`;
             case LinearDensityUnits.MilligramsPerMillimeter:
@@ -598,6 +742,12 @@ export class LinearDensity extends BaseUnit {
                 return `mg/m`;
             case LinearDensityUnits.KilogramsPerMeter:
                 return `kg/m`;
+            case LinearDensityUnits.MicrogramsPerFoot:
+                return `μg/ft`;
+            case LinearDensityUnits.MilligramsPerFoot:
+                return `mg/ft`;
+            case LinearDensityUnits.KilogramsPerFoot:
+                return `kg/ft`;
         default:
             break;
         }
